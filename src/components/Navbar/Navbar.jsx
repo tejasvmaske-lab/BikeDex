35f8ece4ee905c25131992ffd264e54c18128d4d
tejasvmaske-lab <tejas.v.maske@gmail.com>
2026-06-20 12:49:45 +0530

    navbar usability for smaller screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,26 @@
 import Navbar_Button from "../Navbar_Button/Navbar_Button.jsx";
+import BubbleMenu from "../Navbar/BubbleMenu/BubbleMenu.jsx";
 import "./Navbar.css";
+import { useState } from "react";
 
 function Navbar() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-pill">
         <div className="logo">
             <Navbar_Button />
         </div>
-        <div className="mobile-menu">
-          ☰
-        </div>
+        <div
+  className="mobile-menu"
+  onClick={() => setIsMenuOpen(!isMenuOpen)}
+>
+  {isMenuOpen ? "✕" : "☰"}
+</div>
+
+<BubbleMenu isMenuOpen={isMenuOpen} />
         <div className="nav-links">
           <a href="#">Discover</a>
           <a href="#">Compare</a>
